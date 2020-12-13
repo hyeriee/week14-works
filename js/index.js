fca863e6660eaed182f07667cff4a63c324d748a
hyeriee <71582759+hyeriee@users.noreply.github.com>
2020-12-13 23:48:24 +0900

    upload files

---
+++ js/index.js
@@ -0,0 +1,224 @@
+$(document).ready(function(){
+
+	$(".my-img").hide();
+
+  $(".gradient-rect1").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".gradient-rect1").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect1").removeClass("active")
+   })
+
+
+   $(".gradient-rect2").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".gradient-rect2").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect2").removeClass("active")
+   })
+
+
+   $(".gradient-rect3").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".gradient-rect3").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect3").removeClass("active")
+   })
+
+
+   $(".gradient-rect4").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".gradient-rect4").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect4").removeClass("active")
+   })
+
+
+   $(".gradient-rect5").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".gradient-rect5").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect5").removeClass("active")
+   })
+
+
+   $(".gradient-rect6").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".gradient-rect6").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect6").removeClass("active")
+   })
+
+
+   $(".gradient-rect7").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".gradient-rect7").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect7").removeClass("active")
+   })
+
+
+   $(".gradient-rect8").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".gradient-rect8").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect8").removeClass("active")
+   })
+
+   
+   $(".gradient-rect9").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".my-img").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect9").removeClass("active")
+   })
+
+
+     $(".gradient-rect10").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".my-img").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect10").removeClass("active")
+   })
+
+
+     $(".gradient-rect11").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".my-img").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect11").removeClass("active")
+   })
+
+
+     $(".gradient-rect12").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".my-img").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect12").removeClass("active")
+   })
+
+
+	$(".gradient-rect13").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".my-img").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect13").removeClass("active")
+   })
+
+
+   $(".gradient-rect14").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".my-img").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect14").removeClass("active")
+   })
+
+
+   $(".gradient-rect15").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".my-img").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect15").removeClass("active")
+   })
+
+
+   $(".gradient-rect16").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".my-img").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect16").removeClass("active")
+   })
+
+
+   $(".gradient-rect17").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".gradient-rect17").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect17").removeClass("active")
+   })
+
+
+   $(".gradient-rect18").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".gradient-rect18").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect18").removeClass("active")
+   })
+
+
+   $(".gradient-rect19").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".gradient-rect19").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect19").removeClass("active")
+   })
+
+
+   $(".gradient-rect20").on("mouseover", function() {
+       $(".my-img").show();
+       $(this).addClass("active")
+   })
+
+   $(".gradient-rect20").on("mouseleave",function() {
+       $(".my-img").hide();
+       $(".gradient-rect20").removeClass("active")
+   })
+
+});
